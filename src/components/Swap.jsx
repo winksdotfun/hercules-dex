1d12eb536e392d3948dcc8c46a2c58cd8ed84dc6
kalair22 <kalaivaniravi2208@gmail.com>
2024-10-19 14:56:19 +0530

    images

--- src/components/Swap.jsx
+++ src/components/Swap.jsx
@@ -3,7 +3,12 @@ import React from 'react'
 const Swap = () => {
   return (
     <div>
-      <div className=" rounded-lg shadow-md  sm:w-[496px] w-fit "></div>
+      <div className=" bg-[#141414] rounded-[40px] w-[490px] py-[25px] px-[25px] ">
+        <div>
+          <h1 className='text-white'>Swap</h1>
+          <h1>Connect</h1>
+        </div>
+      </div>
     </div>
   );
 }
