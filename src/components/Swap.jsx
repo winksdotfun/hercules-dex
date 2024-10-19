5ff1ce961bb4458e55160f184cc5ad5e5185c6b9
kalair22 <kalaivaniravi2208@gmail.com>
2024-10-19 14:08:32 +0530

    config

--- src/components/Swap.jsx
+++ src/components/Swap.jsx
@@ -3,9 +3,9 @@ import React from 'react'
 const Swap = () => {
   return (
     <div>
-      
+      <div className=" rounded-lg shadow-md  sm:w-[496px] w-fit "></div>
     </div>
-  )
+  );
 }
 
 export default Swap
