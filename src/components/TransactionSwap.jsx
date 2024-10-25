712673d318d7b31d20536cdf2427195a4bae8d5e
kalair22 <kalaivaniravi2208@gmail.com>
2024-10-25 21:53:37 +0530

    responsive

--- src/components/TransactionSwap.jsx
+++ src/components/TransactionSwap.jsx
@@ -26,8 +26,8 @@ const TransactionSwap = ({
   return (
     <div className="fixed inset-0 flex items-center justify-center bg-black p-2">
       <div className="p-2 sm:p-6 rounded-3xl border-[#6c757d33] border-[.5px] shadow-md w-full sm:w-[490px] md:w-[500px] bg-primaryBg">
-        <div className="mb-3 sm:mb-7 flex justify-between items-center">
-          <h1 className="text-start text-white text-lg sm:text-2xl text-two font-semibold">
+        <div className="mb-2 sm:mb-7 flex justify-between items-center">
+          <h1 className="text-start text-white text-base sm:text-2xl text-two font-semibold">
             Swap
           </h1>
           <IoIosClose
@@ -41,34 +41,31 @@ const TransactionSwap = ({
             <img
               src={fromToken.image}
               alt=""
-              className="sm:w-10 w-7 sm:h-10 h-7 rounded-full object-cover mr-2"
+              className="sm:w-10 w-5 sm:h-10 h-5 rounded-full object-cover mr-2"
             />
             <div className="flex flex-col">
-              <p className="text-white font-semibold focus:outline-none text-[13px] sm:text-base text-left">
+              <p className="text-white font-semibold focus:outline-none text-[12px] sm:text-base text-left">
                 {fromToken.name}
               </p>
-              <h1 className="sm:text-sm text-[11px] font-medium text-two text-balanceText text-start">
+              <h1 className="sm:text-sm text-[10px] font-medium text-two text-balanceText text-start">
                 {inputVal}
               </h1>
             </div>
           </div>
 
-          <FaArrowDown
-            size={20}
-            className="text-notConnectedText items-center ml-1 sm:ml-2.5"
-          />
+          <FaArrowDown className="text-notConnectedText items-center ml-1 sm:ml-2.5 text-base mb-1 sm:mb-0 sm:text-2xl" />
 
           <div className="text-white flex">
             <img
               src={toToken.image}
               alt=""
-              className="sm:w-10 w-7 sm:h-10 h-7 rounded-full object-cover mr-2"
+              className="sm:w-10 w-5 sm:h-10 h-5  rounded-full object-cover mr-2"
             />
             <div className="flex flex-col">
-              <p className="text-white font-semibold focus:outline-none text-[13px] sm:text-base text-left">
+              <p className="text-white font-semibold focus:outline-none text-[12px] sm:text-base text-left">
                 {toToken.name}
               </p>
-              <h1 className="sm:text-sm text-[11px] font-medium text-two text-balanceText text-start">
+              <h1 className="sm:text-sm text-[10px] font-medium text-two text-balanceText text-start">
                 {outputVal}
               </h1>
             </div>
@@ -83,35 +80,34 @@ const TransactionSwap = ({
           Error: You cancelled the swap
         </div> */}
 
-        <div className="text-white my-2 sm:my-6 flex flex-col gap-0.5 sm:gap-1">
+        <div className="text-white my-2 sm:my-6 flex flex-col  gap-1">
           <div className="flex justify-between">
-            <p className="sm:text-[16px] text-[11px] font-normal text-two text-start">
+            <p className="sm:text-[16px] text-[9px] font-normal text-two text-start">
               Minimum received
             </p>
-            <p className="sm:text-[16px] text-[11px] font-normal text-two text-right items-center">
+            <p className="sm:text-[16px] text-[9px] font-normal text-two text-right items-center">
               {formatBalance(outputVal)} {toToken.name}{" "}
               {/* Replace with actual value */}
             </p>
           </div>
-          <div className="flex justify-between gap-5">
-            <p className="sm:text-[16px] text-[11px] font-normal text-two text-start">
+          <div className="flex justify-between gap-2">
+            <p className="sm:text-[16px] text-[9px] font-normal text-two text-start">
               {fromToken.name}/{toToken.name} Rate
             </p>
-            <p className="sm:text-[16px] text-[11px] font-normal text-two text-right flex items-center">
+            <p className="sm:text-[16px] text-[9px] font-normal text-two text-right flex items-center">
               1 {fromToken.name}{" "}
-              <BsArrowRight size={20} className="items-center font-thin mx-1" />
+              <BsArrowRight className="items-center font-thin mx-1  sm:text-xl" />
               {/* Add actual rate */} 44.607 {toToken.name}
             </p>
           </div>
-          <div className="flex justify-between gap-5">
-            <p className="sm:text-[16px] text-[11px] font-normal text-two text-start">
+          <div className="flex justify-between gap-2">
+            <p className="sm:text-[16px] text-[9px] font-normal text-two text-start">
               {toToken.name}/{fromToken.name} Rate
             </p>
-            <p className="sm:text-[16px] text-[11px] font-normal text-two text-right flex items-center">
+            <p className="sm:text-[16px] text-[9px] font-normal text-two text-right flex items-center">
               1 {toToken.name}{" "}
-              <BsArrowRight size={20} className="items-center font-thin mx-1" />
+              <BsArrowRight className="items-center font-thin mx-1 sm:text-xl" />
               {/* Add actual rate */} 0.022 {fromToken.name}
-              
             </p>
           </div>
         </div>
