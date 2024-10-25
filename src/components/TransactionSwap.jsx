3631265c06a4c03b7b14110e6b68f34f4f93f615
kalair22 <kalaivaniravi2208@gmail.com>
2024-10-25 21:29:06 +0530

    responsive

--- src/components/TransactionSwap.jsx
+++ src/components/TransactionSwap.jsx
@@ -24,8 +24,8 @@ const TransactionSwap = ({
     return numBalance.toFixed(5);
   };
   return (
-    <div className="fixed inset-0 flex items-center justify-center bg-black py-2">
-      <div className="p-3 sm:p-6 rounded-3xl border-[#6c757d33] border-[.5px] shadow-md w-fit sm:w-[490px] md:w-[500px] bg-primaryBg">
+    <div className="fixed inset-0 flex items-center justify-center bg-black p-2">
+      <div className="p-2 sm:p-6 rounded-3xl border-[#6c757d33] border-[.5px] shadow-md w-full sm:w-[490px] md:w-[500px] bg-primaryBg">
         <div className="mb-3 sm:mb-7 flex justify-between items-center">
           <h1 className="text-start text-white text-lg sm:text-2xl text-two font-semibold">
             Swap
@@ -36,7 +36,7 @@ const TransactionSwap = ({
             onClick={onClose} // Close functionality
           />
         </div>
-        <div className="flex flex-col gap-3">
+        <div className="flex flex-col gap-1.5 sm:gap-3">
           <div className="text-white flex">
             <img
               src={fromToken.image}
@@ -55,7 +55,7 @@ const TransactionSwap = ({
 
           <FaArrowDown
             size={20}
-            className="text-notConnectedText items-center ml-2.5"
+            className="text-notConnectedText items-center ml-1 sm:ml-2.5"
           />
 
           <div className="text-white flex">
@@ -83,7 +83,7 @@ const TransactionSwap = ({
           Error: You cancelled the swap
         </div> */}
 
-        <div className="text-white my-6 flex flex-col gap-1">
+        <div className="text-white my-2 sm:my-6 flex flex-col gap-0.5 sm:gap-1">
           <div className="flex justify-between">
             <p className="sm:text-[16px] text-[11px] font-normal text-two text-start">
               Minimum received
@@ -93,22 +93,25 @@ const TransactionSwap = ({
               {/* Replace with actual value */}
             </p>
           </div>
-          <div className="flex justify-between">
+          <div className="flex justify-between gap-5">
             <p className="sm:text-[16px] text-[11px] font-normal text-two text-start">
               {fromToken.name}/{toToken.name} Rate
             </p>
             <p className="sm:text-[16px] text-[11px] font-normal text-two text-right flex items-center">
-              1 {fromToken.name} = {/* Add actual rate */} 44.607 {toToken.name}
+              1 {fromToken.name}{" "}
               <BsArrowRight size={20} className="items-center font-thin mx-1" />
+              {/* Add actual rate */} 44.607 {toToken.name}
             </p>
           </div>
-          <div className="flex justify-between">
+          <div className="flex justify-between gap-5">
             <p className="sm:text-[16px] text-[11px] font-normal text-two text-start">
               {toToken.name}/{fromToken.name} Rate
             </p>
             <p className="sm:text-[16px] text-[11px] font-normal text-two text-right flex items-center">
-              1 {toToken.name} = {/* Add actual rate */} 0.022 {fromToken.name}
+              1 {toToken.name}{" "}
               <BsArrowRight size={20} className="items-center font-thin mx-1" />
+              {/* Add actual rate */} 0.022 {fromToken.name}
+              
             </p>
           </div>
         </div>
